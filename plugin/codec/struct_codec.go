31f7385a3774363723f13835dec8026560ea7860
jesusrj <reginaldo.jesus@gmail.com>
2024-06-04 20:59:23 -0300

    comments

--- plugin/codec/struct_codec.go
+++ plugin/codec/struct_codec.go
@@ -6,6 +6,11 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
+// see:
+// bson/bsoncodec/struct_codec.go
+// bson/decoder.go
+// bson/unmarshal.go
+
 type StructCodec struct {
 	cache  sync.Map // map[reflect.Type]*structDescription
 	parser StructTagParser
